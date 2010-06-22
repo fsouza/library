ac1ab7f12de1eae221ef02d3183b331084a8fc2b
Francisco Souza <francisco@franciscosouza.net>
2010-06-22 08:10:15 -0300

    Database isolation with Mock

--- loans/features/steps.py
+++ loans/features/steps.py
@@ -1,6 +1,8 @@
-from loans.models import Book
 from freshen import *
 from django.test.client import Client
+
+from mock import Mock
+from mock import patch
 
 @Given('there are (\d+) books in the system')
 def create_books(total_of_books):
@@ -9,13 +11,16 @@ def create_books(total_of_books):
         book = Book()
         book.title = 'Book %d' %(i + 1)
         book.author = 'Francisco Souza'
-        book.save()
         scc.books.append(book)
 
 @When('I navigate to the books list page')
 def navigate_to_books_list():
+    with patch('loans.models.Book') as MockClass:
+        Book.objects.all = Mock()
+        Book.objects.all.return_value = scc.books
     client = Client()
     scc.response = client.get('/loans/books')
+    Book.objects.all.assert_called_with()
 
 @Then('I should see the title of the (\d+) books')
 def check_presence_of_the_title_of_books(total_of_books):
