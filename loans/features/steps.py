a64344fbfacb3a616a6b178662a326dfc6d9df56
Francisco Souza <francisco@franciscosouza.net>
2010-06-21 22:19:17 -0300

    Defined the second step and make it pass

--- loans/features/steps.py
+++ loans/features/steps.py
@@ -1,6 +1,8 @@
 from freshen import *
 from loans.models import Book
 
+from django.test.client import Client
+
 @Given('there are (\d+) books in the system')
 def create_books(total_of_books):
     scc.books = []
@@ -10,3 +12,8 @@ def create_books(total_of_books):
         book.author = 'Francisco Souza'
         book.save()
         scc.books.append(book)
+
+@When('I navigate to the books list page')
+def navigate_to_books_list():
+    client = Client()
+    scc.response = client.get('/loan/books')
